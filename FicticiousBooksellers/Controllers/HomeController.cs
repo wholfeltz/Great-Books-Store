3594cf50759612f7a12a5e11a3e138c0b0dcee16
wholfeltz <waylandholfeltz@mail.weber.edu>
2014-03-13 21:01:10 -0600

    Revision Day 3

--- FicticiousBooksellers/Controllers/HomeController.cs
+++ FicticiousBooksellers/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using FicticiousBooksellers.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -25,6 +28,34 @@ namespace FicticiousBooksellers.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact(ContactViewModel model)
+        {
+            var fromAddress = "ficticiousbooksellers@gmail.com";
+
+            using (var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress, "fiction1234")
+            })
+            {
+                using (var message = new MailMessage(fromAddress, fromAddress)
+                {
+                    Subject = model.Subject,
+                    Body = model.Email + " sent you the following message:\n\n" + model.Message
+                })
+                {
+                    smtp.Send(message);
+                }
+            }
+            return RedirectToAction("Index");
+
         }
     }
 }
---
+++ FicticiousBooksellers/Models/ContactViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FicticiousBooksellers.Models
+{
+    public class ContactViewModel
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Subject { get; set; }
+
+        [Required]
+        [StringLength(800)]
+        [DataType(DataType.MultilineText)]
+        public string Message { get; set; }
+    }
+
+
+}
